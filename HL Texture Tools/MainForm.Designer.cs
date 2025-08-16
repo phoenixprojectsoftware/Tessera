ca80388f2308486f9ff501a09e5b1edb277b8471
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-08-16 02:35:07 +0100

    Add comment for window title assignment
    Inserted a comment to clarify the section of code where the window title is set based on the build configuration.

--- HL Texture Tools/MainForm.Designer.cs
+++ HL Texture Tools/MainForm.Designer.cs
@@ -1105,11 +1105,14 @@ namespace HLTextureTools
             this.MinimumSize = new System.Drawing.Size(385, 256);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            // Window Title
 #if BETA
             this.Text = "Tessera Beta ";
 #else
             this.Text = "Tessera ";
 #endif
+
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
